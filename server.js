97298af33f440e17391f9519d386e7a1416676f7
vgtamas98 <83426063+vgtamas98@users.noreply.github.com>
2021-12-14 09:17:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = 5000;
 var server = http.createServer(function (req, res) {   //create web server
     if (req.url == '/') { //handle requests
         res.writeHead(200, { 'Content-Type': 'text/html' }); 
-        res.write('<html><body><h1>Hello PAT students !</h1></body></html>');
+        res.write('<html><body><h1>Hello, it is Tams!</h1></body></html>');
         res.end();
     }
     else
